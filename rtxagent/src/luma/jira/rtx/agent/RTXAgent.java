33380ea802482e39fb41e6f0d415a5ea425d262d
luma <stubma@gmail.com>
2014-12-13 14:37:45 +0800

    check rtx

--- rtxagent/src/luma/jira/rtx/agent/RTXAgent.java
+++ rtxagent/src/luma/jira/rtx/agent/RTXAgent.java
@@ -1,5 +1,6 @@
 package luma.jira.rtx.agent;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
@@ -12,11 +13,28 @@ import net.sf.json.JSONObject;
 
 public class RTXAgent {
 	public static void main(String[] args) {
+		// get system
+		String os = System.getProperty("os.name");
+		boolean windows = os.contains("Windows");
+		boolean useRTX = false;
+		if(windows) {
+			File f = new File("C:\\Windows\\system32\\SDKAPIJava.dll");
+			useRTX = f.exists();
+		}
+		
 		// init rtx server api
-		RTXSvrApi rtx = new RTXSvrApi();  
-		if(!rtx.Init()) {
-			System.out.println("failed to init RTX server api");
-			return;
+		RTXSvrApi rtx = null;
+		if(useRTX) {
+			try {
+				rtx = new RTXSvrApi();  
+				if(!rtx.Init()) {
+					System.out.println("failed to init RTX server api");
+					return;
+				}
+			} catch (Throwable e) {
+				useRTX = false;
+				rtx = null;
+			}
 		}
 		
 		// start waiting for client
@@ -30,7 +48,9 @@ public class RTXAgent {
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
-			rtx.UnInit();
+			if(useRTX) {
+				rtx.UnInit();
+			}
 		}
 	}
 	
@@ -71,27 +91,29 @@ public class RTXAgent {
 						read -= 4 + length;
 						
 						// create json
-						try {
-	                        JSONObject json = JSONObject.fromObject(receivedData);
-	                        String msg = json.optString("msg");
-	                        JSONArray receiversArray = json.optJSONArray("receivers");
-	                        StringBuilder sb = new StringBuilder();
-	                        int size = receiversArray.size();
-	                        for(int j = 0; j < size; j++) {
-	                        	if(sb.length() > 0)
-	                        		sb.append(',');
-	                        	sb.append(receiversArray.optString(j));
-	                        }
-	                        String receivers = sb.toString();
-                        	int ret = rtx.sendNotify(receivers, "Jira通知", msg, "0", "0");
-                        	if(ret != 0) {
-                        		System.out.println(String.format("发消息给%s失败: %s", receivers, msg));
-                        	} else {
-                        		System.out.println(String.format("消息成功发送给%s", receivers));
-                        	}
-                        } catch (Exception e) {
-                        	System.out.println(String.format("parse json error for %s", receivedData));
-                        }
+						if(rtx != null) {
+							try {
+		                        JSONObject json = JSONObject.fromObject(receivedData);
+		                        String msg = json.optString("msg");
+		                        JSONArray receiversArray = json.optJSONArray("receivers");
+		                        StringBuilder sb = new StringBuilder();
+		                        int size = receiversArray.size();
+		                        for(int j = 0; j < size; j++) {
+		                        	if(sb.length() > 0)
+		                        		sb.append(',');
+		                        	sb.append(receiversArray.optString(j));
+		                        }
+		                        String receivers = sb.toString();
+	                        	int ret = rtx.sendNotify(receivers, "Jira通知", msg, "0", "0");
+	                        	if(ret != 0) {
+	                        		System.out.println(String.format("发消息给%s失败: %s", receivers, msg));
+	                        	} else {
+	                        		System.out.println(String.format("消息成功发送给%s", receivers));
+	                        	}
+	                        } catch (Exception e) {
+	                        	System.out.println(String.format("parse json error for %s", receivedData));
+	                        }	
+						}
 					}
 					clientSocket.close();
 				} catch (IOException e) {
